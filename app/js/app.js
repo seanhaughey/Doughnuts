0590ebc31c6bc9d1824e53a4e034e4a69579b2c5
Sean Haughey <sdhaughey@gmail.com>
2015-10-09 16:35:16 -0500

    finished project

--- app/js/app.js
+++ app/js/app.js
@@ -9,15 +9,56 @@ $(function() {
 			style: $('style').val(),
 			flavor: $('flavor').val(),
 		},
-		dataType: "jsonp",
+		dataType: "json",
 		method: "GET",
-		success: function(data, textStatus, jqXHR) {
-			for (var i =0; i < data.length; i++) {
-				$('#doughnuts').append("<li>" + data[i]['style'] + " " + data[i]['flavor'] + "</li>")
+		success: function(data) {
+			for (var i = 0; i < data.length; i++) {
+				$('#doughnuts').append("<li id=" + data[i]['id'] + ">" + data[i]['style'] + " " + data[i]['flavor'] + " " + "<input type='button' class='delete' value='Delete'>" + "</li>");
 			}
+			// $('.delete').on('click', function() {
+			// 	console.log($(this).parent().attr('id'));
+			// 	var listId = $(this).parent().attr('id');
+			// 	$.ajax({
+			// 		url: "http://api.doughnuts.ga/doughnuts",
+			// 		data: {
+			// 			id: listId,
+			// 			// style: $(data['style']).val(),
+			// 			// flavor: $(data['flavor']).val()
+			// 		},
+			// 		dataType: "json",
+			// 		method: "DELETE",
+			// 		success: function (data) {
+			// 			console.log("success");
+			// 	  		$(this).parent().remove();
+			// 		}		
+
+			// 	})
+		
+			// });
+
 		}
-
-
 	});
-  
+
+	$("#submit").on("click", function(){
+		var doughnutFlavor = $("#doughnut-flavor").val();
+		var doughnutStyle = $("#doughnut-style").val();
+		$.ajax({
+			url: "http://api.doughnuts.ga/doughnuts",
+			data: {
+				// id: $(this).data("id"),
+				style: doughnutStyle,
+				flavor: doughnutFlavor
+			},
+			dataType: "json",
+			method: "POST",
+			success: function(data) {
+				$('#doughnuts').append("<li>" + doughnutStyle + " " + doughnutFlavor + " " + "<input type='button' class='delete' value='Delete'>" + "</li>");
+				console.log(data);	
+			}
+		})
+			return false;
+	})
+		$(document).on('click', '.delete', function() {
+  		$(this).parent().remove();
+	})
 });
